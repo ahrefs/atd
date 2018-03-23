73267d5458423796a73593ec1a7798f7821221c4
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-03-23 23:31:10 +0800

    Move runtime to own package

---
+++ atdgen-runtime/src/ov_run.mli
@@ -0,0 +1,14 @@
+
+val validate_list
+  : (([> `Index of int ] as 'a) list -> 'b -> 'c option)
+  -> 'a list
+  -> 'b list
+  -> 'c option
+
+val validate_array
+  : (([> `Index of int ] as 'a) list -> 'b -> 'c option)
+  -> 'a list
+  -> 'b array
+  -> 'c option
+
+val validate_option : ('a -> 'b -> 'c option) -> 'a -> 'b option -> 'c option

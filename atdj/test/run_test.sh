2e9a7a8da7685784ae35773ab164e9dbb44970aa
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-03-28 16:47:11 +0800

    Add expect tests for atdj

--- atdj/test/run_test.sh
+++ atdj/test/run_test.sh
@@ -2,7 +2,6 @@
 
 CLASSPATH='.:json.jar:junit-4.8.2.jar'
 
-$1 test.atd -package com.mylife.test
 javac -classpath $CLASSPATH com/mylife/test/*.java
 javac -classpath $CLASSPATH AtdjTest.java
 javadoc -classpath $CLASSPATH -d doc/test -public com.mylife.test

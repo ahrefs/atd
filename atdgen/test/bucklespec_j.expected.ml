edddd7793f6df094e053f87812748d553adeebad
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-03-30 16:00:48 +0800

    Add yojson tests for bucklespec
    Just for comparison to the new bucklescript backend

---
+++ atdgen/test/bucklespec_j.expected.ml
@@ -0,0 +1,225 @@
+(* Auto-generated from "bucklespec.atd" *)
+              [@@@ocaml.warning "-27-32-35-39"]
+
+type valid = Bucklespec_t.valid
+
+type label = Bucklespec_t.label
+
+type labeled = Bucklespec_t.labeled = { flag: valid; lb: label; count: int }
+
+let write_valid = (
+  Yojson.Safe.write_bool
+)
+let string_of_valid ?(len = 1024) x =
+  let ob = Bi_outbuf.create len in
+  write_valid ob x;
+  Bi_outbuf.contents ob
+let read_valid = (
+  Atdgen_runtime.Oj_run.read_bool
+)
+let valid_of_string s =
+  read_valid (Yojson.Safe.init_lexer ()) (Lexing.from_string s)
+let write_label = (
+  Yojson.Safe.write_string
+)
+let string_of_label ?(len = 1024) x =
+  let ob = Bi_outbuf.create len in
+  write_label ob x;
+  Bi_outbuf.contents ob
+let read_label = (
+  Atdgen_runtime.Oj_run.read_string
+)
+let label_of_string s =
+  read_label (Yojson.Safe.init_lexer ()) (Lexing.from_string s)
+let write_labeled : _ -> labeled -> _ = (
+  fun ob x ->
+    Bi_outbuf.add_char ob '{';
+    let is_first = ref true in
+    if !is_first then
+      is_first := false
+    else
+      Bi_outbuf.add_char ob ',';
+    Bi_outbuf.add_string ob "\"flag\":";
+    (
+      write_valid
+    )
+      ob x.flag;
+    if !is_first then
+      is_first := false
+    else
+      Bi_outbuf.add_char ob ',';
+    Bi_outbuf.add_string ob "\"lb\":";
+    (
+      write_label
+    )
+      ob x.lb;
+    if !is_first then
+      is_first := false
+    else
+      Bi_outbuf.add_char ob ',';
+    Bi_outbuf.add_string ob "\"count\":";
+    (
+      Yojson.Safe.write_int
+    )
+      ob x.count;
+    Bi_outbuf.add_char ob '}';
+)
+let string_of_labeled ?(len = 1024) x =
+  let ob = Bi_outbuf.create len in
+  write_labeled ob x;
+  Bi_outbuf.contents ob
+let read_labeled = (
+  fun p lb ->
+    Yojson.Safe.read_space p lb;
+    Yojson.Safe.read_lcurl p lb;
+    let field_flag = ref (Obj.magic (Sys.opaque_identity 0.0)) in
+    let field_lb = ref (Obj.magic (Sys.opaque_identity 0.0)) in
+    let field_count = ref (Obj.magic (Sys.opaque_identity 0.0)) in
+    let bits0 = ref 0 in
+    try
+      Yojson.Safe.read_space p lb;
+      Yojson.Safe.read_object_end lb;
+      Yojson.Safe.read_space p lb;
+      let f =
+        fun s pos len ->
+          if pos < 0 || len < 0 || pos + len > String.length s then
+            invalid_arg "out-of-bounds substring position or length";
+          match len with
+            | 2 -> (
+                if String.unsafe_get s pos = 'l' && String.unsafe_get s (pos+1) = 'b' then (
+                  1
+                )
+                else (
+                  -1
+                )
+              )
+            | 4 -> (
+                if String.unsafe_get s pos = 'f' && String.unsafe_get s (pos+1) = 'l' && String.unsafe_get s (pos+2) = 'a' && String.unsafe_get s (pos+3) = 'g' then (
+                  0
+                )
+                else (
+                  -1
+                )
+              )
+            | 5 -> (
+                if String.unsafe_get s pos = 'c' && String.unsafe_get s (pos+1) = 'o' && String.unsafe_get s (pos+2) = 'u' && String.unsafe_get s (pos+3) = 'n' && String.unsafe_get s (pos+4) = 't' then (
+                  2
+                )
+                else (
+                  -1
+                )
+              )
+            | _ -> (
+                -1
+              )
+      in
+      let i = Yojson.Safe.map_ident p f lb in
+      Atdgen_runtime.Oj_run.read_until_field_value p lb;
+      (
+        match i with
+          | 0 ->
+            field_flag := (
+              (
+                read_valid
+              ) p lb
+            );
+            bits0 := !bits0 lor 0x1;
+          | 1 ->
+            field_lb := (
+              (
+                read_label
+              ) p lb
+            );
+            bits0 := !bits0 lor 0x2;
+          | 2 ->
+            field_count := (
+              (
+                Atdgen_runtime.Oj_run.read_int
+              ) p lb
+            );
+            bits0 := !bits0 lor 0x4;
+          | _ -> (
+              Yojson.Safe.skip_json p lb
+            )
+      );
+      while true do
+        Yojson.Safe.read_space p lb;
+        Yojson.Safe.read_object_sep p lb;
+        Yojson.Safe.read_space p lb;
+        let f =
+          fun s pos len ->
+            if pos < 0 || len < 0 || pos + len > String.length s then
+              invalid_arg "out-of-bounds substring position or length";
+            match len with
+              | 2 -> (
+                  if String.unsafe_get s pos = 'l' && String.unsafe_get s (pos+1) = 'b' then (
+                    1
+                  )
+                  else (
+                    -1
+                  )
+                )
+              | 4 -> (
+                  if String.unsafe_get s pos = 'f' && String.unsafe_get s (pos+1) = 'l' && String.unsafe_get s (pos+2) = 'a' && String.unsafe_get s (pos+3) = 'g' then (
+                    0
+                  )
+                  else (
+                    -1
+                  )
+                )
+              | 5 -> (
+                  if String.unsafe_get s pos = 'c' && String.unsafe_get s (pos+1) = 'o' && String.unsafe_get s (pos+2) = 'u' && String.unsafe_get s (pos+3) = 'n' && String.unsafe_get s (pos+4) = 't' then (
+                    2
+                  )
+                  else (
+                    -1
+                  )
+                )
+              | _ -> (
+                  -1
+                )
+        in
+        let i = Yojson.Safe.map_ident p f lb in
+        Atdgen_runtime.Oj_run.read_until_field_value p lb;
+        (
+          match i with
+            | 0 ->
+              field_flag := (
+                (
+                  read_valid
+                ) p lb
+              );
+              bits0 := !bits0 lor 0x1;
+            | 1 ->
+              field_lb := (
+                (
+                  read_label
+                ) p lb
+              );
+              bits0 := !bits0 lor 0x2;
+            | 2 ->
+              field_count := (
+                (
+                  Atdgen_runtime.Oj_run.read_int
+                ) p lb
+              );
+              bits0 := !bits0 lor 0x4;
+            | _ -> (
+                Yojson.Safe.skip_json p lb
+              )
+        );
+      done;
+      assert false;
+    with Yojson.End_of_object -> (
+        if !bits0 <> 0x7 then Atdgen_runtime.Oj_run.missing_fields p [| !bits0 |] [| "flag"; "lb"; "count" |];
+        (
+          {
+            flag = !field_flag;
+            lb = !field_lb;
+            count = !field_count;
+          }
+         : labeled)
+      )
+)
+let labeled_of_string s =
+  read_labeled (Yojson.Safe.init_lexer ()) (Lexing.from_string s)

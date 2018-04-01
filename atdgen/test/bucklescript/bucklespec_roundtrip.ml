61d90899ce42a762a90b6774fa1b8f98f4ad7143
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-04-01 12:05:13 +0800

    refactor roundtrip to print output

--- atdgen/test/bucklescript/bucklespec_roundtrip.ml
+++ atdgen/test/bucklescript/bucklespec_roundtrip.ml
@@ -1,34 +1,94 @@
 
-let r =
-  { Bucklespec_t.
-    flag = false
-  ; lb = "foo bar"
-  ; count = 123
+type 'a test =
+  { name: string
+  ; to_yojson : 'a -> Yojson.Safe.json
+  ; of_yojson : Yojson.Safe.json -> 'a
+  ; data: 'a
   }
 
-let my_vars : Bucklespec_t.simple_vars =
-  [ `Foo (123, 456)
-  ; `Bar
-  ; `Foobar ()
-  ; `Foo_id (`Id "testing")
-  ]
+type test' = T : 'a test -> test'
+
+type failure =
+  { name: string
+  ; actual: Yojson.Safe.json
+  ; received: (Yojson.Safe.json, exn) result
+  }
+
+let pp_json fmt json =
+  Format.pp_print_string fmt (Yojson.Safe.pretty_to_string ~std:true json)
+
+let pp_received fmt = function
+  | Error e -> Format.fprintf fmt "exn: %s" (Printexc.to_string e)
+  | Ok json -> pp_json fmt json
+
+let test ~name ~yojson ~buckle ~data =
+  T { name
+    ; to_yojson = (fun a -> Yojson.Safe.from_string (yojson a))
+    ; of_yojson = (Atdgen_codec_runtime.decode buckle)
+    ; data
+    }
+
+let run_test (T t) =
+  let json = t.to_yojson t.data in
+  let data' =
+    try
+      Ok (t.of_yojson json)
+    with e ->
+      Error e
+  in
+  if Ok t.data = data' then (
+    Ok ()
+  ) else (
+    Error (
+      { name = t.name
+      ; actual = json
+      ; received =
+          (match data' with
+           | Ok d -> Ok (t.to_yojson d)
+           | Error e -> Error e)
+      }
+    )
+  )
+
+let run_tests tests =
+  let failures =
+    tests
+    |> List.fold_left (fun failures t ->
+      match run_test t with
+      | Ok () -> failures
+      | Error fmt -> fmt::failures
+    ) [] in
+  match failures with
+  | [] -> exit 0
+  | xs ->
+      begin
+        xs
+        |> List.iter (fun f ->
+          Format.eprintf
+            "%s: Roundtrip failed.@.Have:%a@.Decoded:%a@."
+            f.name
+            pp_json f.actual
+            pp_received f.received
+        );
+        exit 1
+      end
 
 let () =
-  let json =
-    Bucklespec_j.string_of_labeled r
-    |> Yojson.Safe.from_string in
-  let r' =
-    Atdgen_codec_runtime.decode
-      Bucklespec_bs.read_labeled
-      json in
-  assert (r = r')
-
-let () =
-  let json =
-    Bucklespec_j.string_of_simple_vars my_vars
-    |> Yojson.Safe.from_string in
-  let my_vars' =
-    Atdgen_codec_runtime.decode
-      Bucklespec_bs.read_simple_vars
-      json in
-  assert (my_vars = my_vars')
+  run_tests
+    [ test ~name:"record"
+        ~yojson:Bucklespec_j.string_of_labeled
+        ~buckle:Bucklespec_bs.read_labeled
+        ~data:{ Bucklespec_t.
+                flag = false
+              ; lb = "foo bar"
+              ; count = 123
+              }
+    ; test ~name:"variant"
+        ~yojson:Bucklespec_j.string_of_simple_vars
+        ~buckle:Bucklespec_bs.read_simple_vars
+        ~data:[ `Foo (123, 456)
+              ; `Bar
+              ; `Foobar ()
+              ; `Foo_id (`Id "testing")
+              ]
+    ]

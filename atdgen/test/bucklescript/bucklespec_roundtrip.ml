533be1a6998a850f84529ec9cc4ab624b709e0c7
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-04-01 11:23:00 +0800

    Add failing test for variants

--- atdgen/test/bucklescript/bucklespec_roundtrip.ml
+++ atdgen/test/bucklescript/bucklespec_roundtrip.ml
@@ -6,6 +6,13 @@ let r =
   ; count = 123
   }
 
+let my_vars : Bucklespec_t.simple_vars =
+  [ `Foo (123, 456)
+  ; `Bar
+  ; `Foobar ()
+  ; `Foo_id (`Id "testing")
+  ]
+
 let () =
   let json =
     Bucklespec_j.string_of_labeled r
@@ -15,3 +22,13 @@ let () =
       Bucklespec_bs.read_labeled
       json in
   assert (r = r')
+
+let () =
+  let json =
+    Bucklespec_j.string_of_simple_vars my_vars
+    |> Yojson.Safe.from_string in
+  let my_vars' =
+    Atdgen_codec_runtime.decode
+      Bucklespec_bs.read_simple_vars
+      json in
+  assert (my_vars = my_vars')

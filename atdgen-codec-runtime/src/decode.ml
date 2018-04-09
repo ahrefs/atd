528d5a933184152aa21a005fb2e9f6321007fc2d
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-04-09 19:32:47 +0700

    Remove unnecessary type def in decode

--- atdgen-codec-runtime/src/decode.ml
+++ atdgen-codec-runtime/src/decode.ml
@@ -1,16 +1,4 @@
-type json =
-  [ `Assoc of (string * json) list
-  | `Bool of bool
-  | `Float of float
-  | `Int of int
-  | `Intlit of string
-  | `List of json list
-  | `Null
-  | `String of string
-  | `Tuple of json list
-  | `Variant of string * json option ]
-
-type 'a t = json -> 'a
+type 'a t = Json.t -> 'a
 
 let make f = f
 

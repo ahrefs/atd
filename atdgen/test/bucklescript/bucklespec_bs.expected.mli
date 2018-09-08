6d09dbf42dfcffa0337cfdb07f2cfca647443392
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-09-08 22:06:43 +0400

    Update tests since they no longer require the -open
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- atdgen/test/bucklescript/bucklespec_bs.expected.mli
+++ atdgen/test/bucklescript/bucklespec_bs.expected.mli
@@ -1,6 +1,40 @@
 (* Auto-generated from "bucklespec.atd" *)
               [@@@ocaml.warning "-27-32-35-39"]
-open Bucklespec_t
+
+type valid = Bucklespec_t.valid
+
+type v2 = Bucklespec_t.v2 =  V1_foo of int | V2_bar of bool 
+
+type v1 = Bucklespec_t.v1 =  V1_foo of bool | V2_bar of int 
+
+type id = Bucklespec_t.id
+
+type 'a simple_var = 'a Bucklespec_t.simple_var
+
+type simple_vars = Bucklespec_t.simple_vars
+
+type 'a same_pair = 'a Bucklespec_t.same_pair
+
+type record_json_name = Bucklespec_t.record_json_name = { foo: int }
+
+type point = Bucklespec_t.point
+
+type 'a param_similar = 'a Bucklespec_t.param_similar = {
+  data: 'a;
+  something: int
+}
+
+type 'a param = 'a Bucklespec_t.param = { data: 'a; nothing: unit }
+
+type ('a, 'b) pair = ('a, 'b) Bucklespec_t.pair = { left: 'a; right: 'b }
+
+type 'a pairs = 'a Bucklespec_t.pairs
+
+type label = Bucklespec_t.label
+
+type labeled = Bucklespec_t.labeled = { flag: valid; lb: label; count: int }
+
+type from_module_a = A_t.from_module_a
 
 val read_valid :  valid Atdgen_codec_runtime.Decode.t
 
@@ -18,13 +52,13 @@ val read_id :  id Atdgen_codec_runtime.Decode.t
 
 val write_id :  id Atdgen_codec_runtime.Encode.t
 
-val read_simple_vars :  simple_vars Atdgen_codec_runtime.Decode.t
-
-val write_simple_vars :  simple_vars Atdgen_codec_runtime.Encode.t
-
 val read_simple_var : 'a Atdgen_codec_runtime.Decode.t -> 'a simple_var Atdgen_codec_runtime.Decode.t
 
 val write_simple_var : 'a Atdgen_codec_runtime.Encode.t -> 'a simple_var Atdgen_codec_runtime.Encode.t
+
+val read_simple_vars :  simple_vars Atdgen_codec_runtime.Decode.t
+
+val write_simple_vars :  simple_vars Atdgen_codec_runtime.Encode.t
 
 val read_same_pair : 'a Atdgen_codec_runtime.Decode.t -> 'a same_pair Atdgen_codec_runtime.Decode.t
 

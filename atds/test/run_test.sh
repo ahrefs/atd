985f3c64196ec8e7cce002d687194316785540ad
Ivan Jager <aij+git@mrph.org>
2018-10-06 08:13:37 -0500

    atds test: run scaladoc

--- atds/test/run_test.sh
+++ atds/test/run_test.sh
@@ -5,9 +5,8 @@ CLASSPATH='.:argonaut_2.12-6.2.2.jar:junit-4.8.2.jar'
 
 scalac -classpath $CLASSPATH test.scala
 scalac -classpath $CLASSPATH AtdsTest.scala
-#javadoc -quiet -Xdoclint:none -classpath $CLASSPATH -d doc/test \
-#        -public com.mylife.test -quiet \
-#    | grep -v "Creating destination directory"
+scaladoc -classpath $CLASSPATH test.scala -d doc 2>&1 |
+    grep -q 'model contains [0-9]* documentable templates'
 scala  -classpath $CLASSPATH AtdsTest > scala.trace || {
     cat scala.trace
     exit 1

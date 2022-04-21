d740f2e91cd669ee8f6e22c35b0fde697af4fca5
Martin Jambon <martin@r2c.dev>
2022-04-21 10:37:31 -0700

    s/python/typescript/
    Co-authored-by: Louis <louis.roche@ahrefs.com>

--- atdts/src/lib/Codegen.mli
+++ atdts/src/lib/Codegen.mli
@@ -2,6 +2,6 @@
    Python code generation for JSON support (no biniou support)
 *)
 
-(** Take ATD type definitions and translate them to Python, writing
-    them out to a file which should have the '.py' extension. *)
+(** Take ATD type definitions and translate them to TypeScript, writing
+    them out to a file which should have the '.ts' extension. *)
 val run_file : string -> unit

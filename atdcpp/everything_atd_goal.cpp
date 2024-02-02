05a4975dcc90f4d7d3c100715687eba4a2e214b1
Alexandre Bourquelot <alexandre.bourquelot@ahrefs.com>
2024-02-02 17:34:36 +0800

    rename

--- atdcpp/everything_atd_goal.cpp
+++ atdcpp/everything_atd_goal.cpp
@@ -12,7 +12,7 @@ IntFloatParametrizedRecord IntFloatParametrizedRecord::from_json(const rapidjson
     if (doc.IsObject())
     {
         const rapidjson::Value &fieldA = doc["field_a"];
-    
+
         record.field_a = _atd_read_int(doc["field_a"]);
         record.field_b = _atd_read_array<float, _atd_read_float>(doc["field_b"]);
     }
@@ -33,7 +33,6 @@ IntFloatParametrizedRecord IntFloatParametrizedRecord::from_json_string(const st
     return from_json(doc);
 }
 
-
 std::string IntFloatParametrizedRecord::to_json_string() const
 {
     rapidjson::StringBuffer buffer;
@@ -50,7 +49,6 @@ std::string IntFloatParametrizedRecord::to_json_string() const
 
     return buffer.GetString();
 }
-
 
 namespace
 {
@@ -78,26 +76,29 @@ std::string NestedNestedIntListRecord::to_json_string() const
 
 namespace __NNNIntListRecord
 {
-    typedef std::vector<std::vector<int>> T;
-
-    void write_0(std::vector<int> obj, rapidjson::Writer<rapidjson::StringBuffer> &writer)
+    namespace field_0
     {
-        _atd_write_array<int, _atd_write_int>(obj, writer);
-    }
+        typedef std::vector<std::vector<int>> T;
 
-    void write(T obj, rapidjson::Writer<rapidjson::StringBuffer> &writer)
-    {
-        _atd_write_array<std::vector<int>, write_0>(obj, writer);
-    }
+        void write_0(std::vector<int> obj, rapidjson::Writer<rapidjson::StringBuffer> &writer)
+        {
+            _atd_write_array<int, _atd_write_int>(obj, writer);
+        }
 
-    std::vector<int> read_0(const rapidjson::Value &val)
-    {
-        return _atd_read_array<int, _atd_read_int>(val);
-    }
+        void write(T obj, rapidjson::Writer<rapidjson::StringBuffer> &writer)
+        {
+            _atd_write_array<std::vector<int>, write_0>(obj, writer);
+        }
 
-    T read(const rapidjson::Value &val)
-    {
-        return _atd_read_array<std::vector<int>, read_0>(val);
+        std::vector<int> read_0(const rapidjson::Value &val)
+        {
+            return _atd_read_array<int, _atd_read_int>(val);
+        }
+
+        T read(const rapidjson::Value &val)
+        {
+            return _atd_read_array<std::vector<int>, read_0>(val);
+        }
     }
 }
 
@@ -110,14 +111,12 @@ std::string NNNIntListRecord::to_json_string() const
 
     writer.Key("field_a");
 
-    _atd_write_array<std::vector<std::vector<int>>, __NNNIntListRecord::write>(field_a, writer);
+    _atd_write_array<std::vector<std::vector<int>>, __NNNIntListRecord::field_0::write>(field_a, writer);
 
     writer.EndObject();
 
     return buffer.GetString();
 }
-
-
 
 NNNIntListRecord NNNIntListRecord::from_json(const rapidjson::Document &doc)
 {
@@ -125,34 +124,45 @@ NNNIntListRecord NNNIntListRecord::from_json(const rapidjson::Document &doc)
     if (doc.IsObject())
     {
         const rapidjson::Value &fieldA = doc["field_a"];
-    
-        record.field_a = _atd_read_array<std::vector<std::vector<int>>, __NNNIntListRecord::read>(doc["field_a"]);
+
+        record.field_a = _atd_read_array<std::vector<std::vector<int>>, __NNNIntListRecord::field_0::read>(doc["field_a"]);
     }
 
     return record;
 }
 
+const rapidjson::Document doc_from_json(const std::string &json)
+{
+    rapidjson::Document doc;
+    doc.Parse(json.c_str());
+
+    if (doc.HasParseError())
+    {
+        throw AtdException("Error parsing JSON: " + std::string(rapidjson::GetParseError_En(doc.GetParseError())));
+    }
+
+    return doc;
+}
+
 int main()
 {
-    std::string json = R"({"field_a":1134,"field_b":[1.1,2.2,7]})";
+    std::string NNNIntListRecord_json = R"({"field_a":[[[1,2,3],[4,4,6]],[[7,8,9],[10,11,12]]]})";
 
-    try
+    NNNIntListRecord recordFromJson = NNNIntListRecord::from_json(doc_from_json(NNNIntListRecord_json));
+    /// iterate through all vectors to print all values (3 level of nesting)
+    for (auto &vec1 : recordFromJson.field_a)
     {
-        IntFloatParametrizedRecord recordFromJson = IntFloatParametrizedRecord::from_json_string(json);
-        std::cout << "IntFloatParametrizedRecord: " << IntFloatParametrizedRecord{123, {1.1, 2.2, 3.3}}.to_json_string() << std::endl;
-
-        std::cout << "field_a: " << recordFromJson.field_a << std::endl;
-        std::cout << "field_b: ";
-        for (auto &val : recordFromJson.field_b)
+        for (auto &vec2 : vec1)
         {
-            std::cout << val << " ";
+            for (auto &val : vec2)
+            {
+                std::cout << val << " ";
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+
+    std::cout << "NNNIntListRecord: " << NNNIntListRecord{{{{1, 2, 3}, {4, 5, 6}}, {{7, 8, 9}, {10, 18, 12}}}}.to_json_string() << std::endl;
 
     return 0;
 }

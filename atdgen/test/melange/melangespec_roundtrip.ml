aa715a7019dbe498c6e6d9d140251145f4a51147
Martin Jambon <martin@semgrep.com>
2024-04-04 15:15:51 -0700

    Remove unused 'open'

--- atdgen/test/melange/melangespec_roundtrip.ml
+++ atdgen/test/melange/melangespec_roundtrip.ml
@@ -161,9 +161,9 @@ let () =
     ; test_decode ~name:"decode list open enum"
         ~yojson:Melangespec_j.string_of_with_open_enum_list
         ~melange:Melangespec_mel.read_with_open_enum_list
-        ~data: Melangespec_t.([`Alpha; `Other "other"])
+        ~data: [`Alpha; `Other "other"]
     ; test_encode ~name:"encode list open enum"
         ~yojson:Melangespec_j.with_open_enum_list_of_string
         ~melange:Melangespec_mel.write_with_open_enum_list
-        ~data: Melangespec_t.([`Alpha; `Other "other"])
+        ~data: [`Alpha; `Other "other"]
     ]

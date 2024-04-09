877dabaf772648e41013c88d81a1f3b784cbc2d3
Alexandre Bourquelot <alexandre.bourquelot@ahrefs.com>
2024-04-09 10:41:42 +0800

    atdcpp: remove dlang mentions

--- atdcpp/src/lib/Codegen.mli
+++ atdcpp/src/lib/Codegen.mli
@@ -1,7 +1,7 @@
 (*
-   Dlang code generation for JSON support (no biniou support)
+   C++ code generation for JSON support (no biniou support)
 *)
 
-(** Take ATD type definitions and translate them to Dlang, writing
+(** Take ATD type definitions and translate them to C++, writing
     them out to a file which should have the '.d' extension. *)
 val run_file : string -> unit

b749bbee2fc6ef0bc9a62b7878b7da93b418203d
Martin Jambon <martin@mjambon.com>
2026-03-30 00:27:30 -0700

    Fix examples

--- atd-jsonlike/src/Number.mli
+++ atd-jsonlike/src/Number.mli
@@ -2,19 +2,19 @@
 
     For the JSON literal [42], the fields should be all filled as follows:
 {v
-      { int = Some 42; float = Some 42.; literal = "42" }
+      { int = Some 42; float = Some 42.; literal = Some "42" }
 v}
     JSON numbers that can be represented as an OCaml float (IEEE 754
     binary64) but not as an int will look like this:
 {v
-      { int = None; float = Some 1.2; literal = "1.2" }
+      { int = None; float = Some 1.2; literal = Some "1.2" }
 v}
     In rare cases, a number literal may be valid syntactically but can't
     be represented as an int or a float. In this case, the JSON literal
     may still be used to convert the number to an int64 or to something
     else. Here's an example:
 {v
-      { int = None; float = None; literal = "1e400" }
+      { int = None; float = None; literal = Some "1e400" }
 v}
 
     Rules:
